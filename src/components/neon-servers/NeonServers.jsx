a8dd2866809d02375ea3da6d2659218f058cb6c4
dawitmanukian <davit.manukyan.d@tumo.org>
2023-08-26 13:22:42 +0400

    add some styles

--- src/components/neon-servers/NeonServers.jsx
+++ src/components/neon-servers/NeonServers.jsx
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './neon-servers.css';
 import CopyToClipboard from 'react-copy-to-clipboard';
 
 const NeonServers = ({servers}) => {
+
+    const [isCopied, setIsCopied] = useState(false)
+
   return (
     <div>
         <h1>СЕРВЕРА</h1>
         <div className='all-servers'>
+            {isCopied && <span className='servers-success'>IP адрес скопирован !</span>}
+
             {servers.map(el => {
                 return (
                     <CopyToClipboard text={el.ip}>
-                        <div className='server' onClick={() => alert('Copied !')}>
+                        <div className='server' onClick={() => {
+                            setIsCopied(true)
+                            setTimeout(() => {
+                                setIsCopied(false)
+                            }, 2500);
+                        }}>
                             <div className='server-data'>
                                 <h5>{el.name}</h5>
                                 <b>{el.members} / {el.max}</b>
-                                    <button className='api-btn'>Скопировать IP</button>
+                                <button className='api-btn'>Скопировать IP</button>
+                                {isCopied && <span>Copied !</span>}
                             </div>  
                             <div className='server-number'>
                                 <b>
